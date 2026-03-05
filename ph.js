3b99fdbfe16fbb4c2088e2e279f373cf286918a0
Parth Valand <parthvaland@Parths-MacBook-Air.local>
2026-03-05 23:30:26 +0530

    Final Portfolio Website

--- ph.js
+++ ph.js
@@ -1,32 +1,91 @@
-document.getElementById("contactForm").addEventListener("submit", async function(e) {
-    e.preventDefault();
+function myMenuFunction() {
+    var menu = document.getElementById("myNavMenu");
+    menu.classList.toggle("responsive");
+}
 
-    const formData = {
-        name: this.name.value,
-        email: this.email.value,
-        message: this.message.value
-    };
+window.onscroll = function () { headerShadow() };
 
-    try {
-        const response = await fetch("https://portfolio-backend-r8c5.onrender.com/send", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(formData)
-        });
+function headerShadow() {
+    const navHeader = document.getElementById("header");
 
-        const data = await response.json();
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
 
-        if (data.success) {
-            alert(" Message Sent Successfully!");
-            this.reset();
-        } else {
-            alert(" Failed to send message.");
-        }
+        navHeader.style.boxShadow = "0 1px 6px rgba(0,0,0,0.1)";
+        navHeader.style.height = "70px";
 
-    } catch (error) {
-        console.error(error);
-        alert("Server Error. Try again later.");
+    } else {
+
+        navHeader.style.boxShadow = "none";
+        navHeader.style.height = "80px";
     }
+}
+const toggle = document.getElementById("darkToggle");
+
+toggle.addEventListener("click", () => {
+document.body.classList.toggle("dark-mode");
+});
+
+ScrollReveal().reveal('.featured-text', { delay: 200 });
+ScrollReveal().reveal('.featured-image', { delay: 400 });
+ScrollReveal().reveal('.project-box', { interval: 200 });
+ScrollReveal().reveal('.skills-box', { interval: 200 });
+(function(){
+emailjs.init("hhEa9uBrETMRJVNlm");
+})();
+
+const form = document.getElementById("contactForm");
+const message = document.getElementById("formMessage");
+
+form.addEventListener("submit", function(e){
+
+e.preventDefault();
+
+emailjs.send("service_rcsf3i8","template_i49rpkq",{
+
+name: this.name.value,
+email: this.email.value,
+message: this.message.value
+
+})
+.then(function(){
+
+message.innerHTML = "✅ Message sent successfully!";
+form.reset();
+
+}, function(error){
+
+message.innerHTML = "❌ Failed to send message.";
+
+});
+
+});
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-link");
+
+window.addEventListener("scroll", () => {
+
+let current = "";
+
+sections.forEach((section) => {
+
+const sectionTop = section.offsetTop;
+const sectionHeight = section.clientHeight;
+
+if(pageYOffset >= sectionTop - 200){
+current = section.getAttribute("id");
+}
+
+});
+
+navLinks.forEach((link) => {
+
+link.classList.remove("active");
+
+if(link.getAttribute("href") === "#" + current){
+link.classList.add("active");
+}
+
+});
+
 });
